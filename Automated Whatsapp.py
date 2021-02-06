b791c300003b1996488a0b584cb284f034d2ddb5
Kushankur Das <kushankurdas53543@gmail.com>
2021-02-06 22:34:22 +0530

    Updated

--- Automated Whatsapp.py
+++ Automated Whatsapp.py
@@ -1,3 +1,3 @@
 # install pywhatkit by pip install pywhatkit command into terminal
 import pywhatkit
-pywhatkit.sendwhatmsg('+919477091580', 'Happy Birthday', 22, 25)
+pywhatkit.sendwhatmsg('phone number', 'message', hrs, mins)
